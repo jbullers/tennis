ef869cd068c3291cc03689710dbf0dc2c6a4e3f5
Jason Bullers <jason.bullers@gmail.com>
2022-05-03 21:34:11 -0400

    Give domain names to "player1" and "player2": "server" and "receiver". Fix check for player name in `wonPoint`. Use `serverName` and `receiverName` in output strings (e.g. "Advantage ..."). Reformat code.

--- src/main/java/com/github/jbullers/tennis/TennisGame1.java
+++ src/main/java/com/github/jbullers/tennis/TennisGame1.java
@@ -1,19 +1,19 @@
 package com.github.jbullers.tennis;
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String serverName;
+    private final String receiverName;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String serverName, String receiverName) {
+        this.serverName = serverName;
+        this.receiverName = receiverName;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equalsIgnoreCase(serverName))
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -21,53 +21,48 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+        if (m_score1 == m_score2) {
+            switch (m_score1) {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage " + serverName;
+            else if (minusResult == -1) score = "Advantage " + receiverName;
+            else if (minusResult >= 2) score = "Win for " + serverName;
+            else score = "Win for " + receiverName;
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = m_score1;
+                else {
+                    score += "-";
+                    tempScore = m_score2;
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
