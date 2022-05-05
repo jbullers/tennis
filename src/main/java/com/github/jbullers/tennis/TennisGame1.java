68b86fae0dad78d53b21a2863bed8b9f306aeab3
Jason Bullers <jason.bullers@gmail.com>
2022-05-04 23:56:17 -0400

    Use state machine to handle scoring. The meat of the logic is in the NumericalScore state since from there, many transitions are possible (other state transitions are comparatively very straight forward). Arguably, instead of being heavy on logic here, additional states could be introduced, but then we have a combinatorial explosion of all possible scores (granted it's not _huge_, but still...). I think it's an interesting approach (and maybe can be tightened up a bit), especially since for the most part, each state just describes what's next depending on who scores, and what the textual representation (the "call") should be. Is it better or easier to understand than the previous version? Probably not.

--- src/main/java/com/github/jbullers/tennis/TennisGame1.java
+++ src/main/java/com/github/jbullers/tennis/TennisGame1.java
@@ -2,10 +2,9 @@ package com.github.jbullers.tennis;
 
 public class TennisGame1 implements TennisGame {
 
-    private int serverScore;
-    private int receiverScore;
     private final String serverName;
     private final String receiverName;
+    private Score score = new LoveAll();
 
     public TennisGame1(String serverName, String receiverName) {
         this.serverName = serverName;
@@ -13,38 +12,180 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equalsIgnoreCase(serverName))
-            serverScore += 1;
-        else
-            receiverScore += 1;
-    }
-
-    public String getScore() {
-        if (serverScore == receiverScore) {
-            return switch (serverScore) {
-                case 0 -> "Love-All";
-                case 1 -> "Fifteen-All";
-                case 2 -> "Thirty-All";
-                default -> "Deuce";
-            };
-        } else if (serverScore >= 4 || receiverScore >= 4) {
-            int minusResult = serverScore - receiverScore;
-            if (minusResult == 1) return "Advantage " + serverName;
-            else if (minusResult == -1) return "Advantage " + receiverName;
-            else if (minusResult >= 2) return "Win for " + serverName;
-            else return "Win for " + receiverName;
+        if (playerName.equalsIgnoreCase(serverName)) {
+            score = score.serverPoint();
         } else {
-            return callForScore(serverScore) + "-" + callForScore(receiverScore);
+            score = score.receiverPoint();
         }
     }
 
-    private static String callForScore(int score) {
-        return switch (score) {
-            case 0 -> "Love";
-            case 1 -> "Fifteen";
-            case 2 -> "Thirty";
-            case 3 -> "Forty";
-            default -> throw new IllegalArgumentException("Expected score of 0-3 inclusive");
-        };
+    public String getScore() {
+        return score.callForScore(serverName, receiverName);
+    }
+
+    private sealed interface Score {
+        Score serverPoint();
+
+        Score receiverPoint();
+
+        String callForScore(String serverName, String receiverName);
+    }
+
+    private record LoveAll() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new NumericalScore(1, 0);
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new NumericalScore(0, 1);
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Love-All";
+        }
+    }
+
+    private record NumericalScore(int serverScore, int receiverScore) implements Score {
+
+        @Override
+        public Score serverPoint() {
+            int updatedServerScore = serverScore + 1;
+            int delta = updatedServerScore - receiverScore;
+            if (delta == 0 && updatedServerScore >= 3) {
+                return new Deuce();
+            } else if (delta == 1 && updatedServerScore >= 4) {
+                return new AdvantageServer();
+            } else if (delta >= 2 && updatedServerScore >= 4) {
+                return new WinServer();
+            } else {
+                return new NumericalScore(updatedServerScore, receiverScore);
+            }
+        }
+
+        @Override
+        public Score receiverPoint() {
+            int updatedReceiverScore = receiverScore + 1;
+            int delta = updatedReceiverScore - serverScore;
+            if (delta == 0 && updatedReceiverScore >= 3) {
+                return new Deuce();
+            } else if (delta == 1 && updatedReceiverScore >= 4) {
+                return new AdvantageReceiver();
+            } else if (delta >= 2 && updatedReceiverScore >= 4) {
+                return new WinReceiver();
+            } else {
+                return new NumericalScore(serverScore, updatedReceiverScore);
+            }
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return serverScore != receiverScore ?
+                    callForScore(serverScore) + "-" + callForScore(receiverScore) :
+                    callForScore(serverScore) + "-All";
+        }
+
+        private static String callForScore(int score) {
+            return switch (score) {
+                case 0 -> "Love";
+                case 1 -> "Fifteen";
+                case 2 -> "Thirty";
+                case 3 -> "Forty";
+                default -> throw new IllegalArgumentException("Expected score of 0-3 inclusive");
+            };
+        }
+    }
+
+    private record Deuce() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new AdvantageServer();
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new AdvantageReceiver();
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Deuce";
+        }
+    }
+
+    private record AdvantageServer() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new WinServer();
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new Deuce();
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Advantage " + serverName;
+        }
+    }
+
+    private record AdvantageReceiver() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new Deuce();
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new WinReceiver();
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Advantage " + receiverName;
+        }
+    }
+
+    private record WinServer() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new WinServer();
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new WinServer();
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Win for " + serverName;
+        }
+    }
+
+    private record WinReceiver() implements Score {
+
+        @Override
+        public Score serverPoint() {
+            return new WinReceiver();
+        }
+
+        @Override
+        public Score receiverPoint() {
+            return new WinReceiver();
+        }
+
+        @Override
+        public String callForScore(String serverName, String receiverName) {
+            return "Win for " + receiverName;
+        }
     }
 }
